d063b3bd9552c74c1e714a8f08c5aaee3212c36e
Madan elriano <fillar72@gmail.com>
2026-02-08 10:55:43 +0700

    feat: Create EffectsPanel component

---
+++ src/components/EffectsPanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const EffectsPanel = () => {
+  return (
+    <div className="effects-panel-container">
+      {/* List of video effects and controls */}
+      <h3>Effects</h3>
+      <ul>
+        <li>Grayscale</li>
+        <li>Sepia</li>
+        <li>Blur</li>
+      </ul>
+    </div>
+  );
+};
+
+export default EffectsPanel;
