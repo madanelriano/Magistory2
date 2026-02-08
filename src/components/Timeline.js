7e505222d7ef6ca647441eda039d9e6f3cf4aaaf
Madan elriano <fillar72@gmail.com>
2026-02-08 10:55:36 +0700

    feat: Create Timeline component

---
+++ src/components/Timeline.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Timeline = () => {
+  return (
+    <div className="timeline-container">
+      {/* Timeline for video segments */}
+      <p>Video Timeline</p>
+    </div>
+  );
+};
+
+export default Timeline;
