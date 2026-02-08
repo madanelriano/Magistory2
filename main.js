517d094a7620b9c9e463a15c3f22a6f2971e32d8
Madan elriano <fillar72@gmail.com>
2026-02-08 22:46:33 +0700

    Wrap application with VideoEditorProvider in main.js

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { VideoEditorProvider } from './src/contexts/VideoEditorContext';
 import EditorPage from './src/pages/EditorPage';
 import './src/styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<EditorPage />} />
-        {/* Add more routes here if needed */}
-      </Routes>
-    </Router>
+    <VideoEditorProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<EditorPage />} />
+          {/* Add more routes here if needed */}
+        </Routes>
+      </Router>
+    </VideoEditorProvider>
   </React.StrictMode>
 );
