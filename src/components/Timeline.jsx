cf776f171d7e6d83c64951dfb0489040db2f5e2d
Madan elriano <fillar72@gmail.com>
2026-02-08 20:50:21 +0700

    Create Timeline.jsx component

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Timeline() {
+  return (
+    <div className="timeline-container" style={{ border: '1px solid #00bcd4', padding: '10px', marginTop: '20px', backgroundColor: '#2a2a2a' }}>
+      <h3>Timeline</h3>
+      {/* Timeline elements will go here */}
+      <div style={{ height: '80px', background: '#3a3a3a', borderRadius: '5px' }}>
+        {/* Visual representation of video segments */}
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
